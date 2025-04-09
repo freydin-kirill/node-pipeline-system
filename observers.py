bae7e8b0990d6cf2b39ef18561b9d1f78f35d498
Kirill <freydin.kirill@gmail.com>
2025-04-09 15:24:06 +0300

    Fix

--- observers.py
+++ observers.py
@@ -1,19 +1,22 @@
 from abc import abstractmethod, ABC
+from typing import TYPE_CHECKING
 
-from nodes import BaseNode
+if TYPE_CHECKING:
+    from nodes import BaseNode
 
 class BaseObserver(ABC):
     @abstractmethod
-    def update(self, node: BaseNode):
+    def update(self, node: 'BaseNode'):
         raise NotImplementedError
 
 
 class LoggerObserver(BaseObserver):
-    def update(self, node: BaseNode):
-        print(f'Logger: Node {node.name} changed status to {node.status}')
+    def update(self, node: 'BaseNode'):
+        print(f'Logger: Node {node.name} changed status to \"{node.status.value}\"')
 
 
 class CascadeObserver(BaseObserver):
-    def update(self, node: BaseNode):
+    def update(self, node: 'BaseNode'):
         for child in node.children:
             child.status = node.status
+
